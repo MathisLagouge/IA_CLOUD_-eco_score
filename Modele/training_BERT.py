4dd08131f63e1a734172684c8e3709cec7f10cb1
MathisLagouge <102744090+MathisLagouge@users.noreply.github.com>
2024-01-12 11:29:27 +0100

    traininf file for model

---
+++ Modele/training_BERT.py
@@ -0,0 +1,15 @@
+#import function
+from creerBERT import *
+
+#param for training
+NUM_LABEL = 3
+BATCH_SIZE = 4
+TEST_SIZE = 0.2
+NUM_EPOCH = 5
+DATA_PATH = "data/restaurant_stars_int_eq.csv"
+SAVE_PATH = "save_trained_model"
+
+#model training
+#save the trained model
+#print model accuracy 
+train_bert(BATCH_SIZE,NUM_EPOCH,TEST_SIZE,DATA_PATH, SAVE_PATH)
