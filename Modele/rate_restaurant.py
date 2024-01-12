99a1c94cee54dd481f54cf254a64b9e36d00be1b
GindroQuentin <107125608+GindroQuentin@users.noreply.github.com>
2024-01-12 11:26:17 +0100

    Ajout des commentaires

--- Modele/rate_restaurant.py
+++ Modele/rate_restaurant.py
@@ -1,17 +1,25 @@
+# import les packages
 import pandas as pd
 from Prediction import prediction, score
 from datasets import load_dataset
 
+# Categories que l'on note
 CATEGORIES = ["organic", "climate", "water", "social", "governance", "waste", "adverse"]
+# Dataframe final
 DF = {}
+# Chemin du modèle que l'on utilise
 MODEL_PATH = "trained_model"
 
+# On cree un fichier csv pour la ville, regroupant toutes les notes des restaurants recuperes par categories et la note globale
 def creation_csv(city):
+    # Pour chaque categorie
     for i in range(len(CATEGORIES)):
         print("Traitement de la categorie ", CATEGORIES[i])
+        # On charge le csv associe
         datas = load_dataset("csv", data_files=str("Scrapping/categories/" + city + "/" + CATEGORIES[i] + ".csv"))["train"]
         liste_restaurant = []
         iter = 0
+        # On recupere tous les restaurants et leurs reviews
         while(iter < len(datas["PLACE"])):
             if datas[iter]["PLACE"] not in liste_restaurant:
                 avis = []
@@ -28,6 +36,7 @@ def creation_csv(city):
                     DF[liste_restaurant[-1]]["Review_"+CATEGORIES[i]] = res
             iter += 1
 
+    # On cree un dictionnaire contenant sur chaque ligne, le nom du restaurant et les notes associees
     final = {"PLACE" : [],
             "organic" : [],
             "climate" : [],
@@ -50,5 +59,6 @@ def creation_csv(city):
                 final[y].append(None)
         final["mean"].append(mean / nb_categories)
 
+    # On enregistre le dataframe dans un csv
     dataframe = pd.DataFrame(final)
     dataframe.to_csv("Scrapping/categories/" + city + "/notation_restaurant.csv", index=False)
